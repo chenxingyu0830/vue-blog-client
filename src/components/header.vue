81d7524a426b0863d12ebe3454c561c838ef2a42
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-07-24 00:36:12 +0800

    Ref: 样式调整

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,24 @@
 <template>
-  <header :class="{login: isLogin, 'no-login': !isLogin}">
+  <header :class="{ login: isLogin, 'no-login': !isLogin }">
     <template v-if="!isLogin">
       <h1>Let's share of client-blog</h1>
       <div class="btns">
         <router-link to="/login"><el-button>立即登录</el-button></router-link>
-        <router-link to="/register"><el-button>注册账号</el-button></router-link>
+        <router-link to="/register"
+          ><el-button>注册账号</el-button></router-link
+        >
       </div>
     </template>
     <template v-if="isLogin">
       <h1><router-link to="/">Let's share</router-link></h1>
       <router-link to="/create"><i class="edit el-icon-plus"></i></router-link>
       <div class="user">
-        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username">          
+        <img
+          class="avatar"
+          :src="user.avatar"
+          :alt="user.username"
+          :title="user.username"
+        />
         <ul>
           <li><router-link to="/my">我的</router-link></li>
           <li><a href="#" @click="onLogout">注销</a></li>
@@ -19,45 +26,39 @@
       </div>
     </template>
   </header>
-  
 </template>
 
 <script>
+import auth from "@/api/auth";
+window.auth = auth;
 
-  import auth from '@/api/auth'
-  window.auth = auth
+import { mapActions, mapGetters } from "vuex";
 
-  import { mapState, mapActions, mapGetters } from 'vuex'
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["isLogin", "user"]),
+  },
 
-  export default {
-    data() {
-      return {}
+  created() {
+    this.checkLogin();
+  },
+
+  methods: {
+    ...mapActions(["checkLogin", "logout"]),
+
+    onLogout() {
+      this.logout();
     },
-    computed: {
-      ...mapGetters(['isLogin','user'])
-    },
-
-    created() {
-      this.checkLogin()
-    },
-
-
-    methods: {
-      ...mapActions(['checkLogin','logout']),
-
-      onLogout() {
-        this.logout()
-      }
-    },
-
-
-  }
+  },
+};
 </script>
 
 
 <style lang="less" scoped>
-
-@import "../assets/base.less";  
+@import "../assets/base.less";
 
 header.no-login {
   padding: 0 12% 30px 12%;
@@ -80,13 +81,11 @@ header.no-login {
     margin: 15px 0 0 0;
     color: #fff;
   }
-  
 
   button {
     margin: 20px 5px 0;
   }
-} 
-
+}
 
 header.login {
   display: flex;
@@ -102,7 +101,7 @@ header.login {
 
     a {
       color: #fff;
-      }
+    }
   }
 
   .edit {
@@ -122,14 +121,15 @@ header.login {
   }
 
   .user {
-  position: relative;
-    img{
+    position: relative;
+    img {
       transition: all 0.5s;
       &:hover {
-        transform: scale(1.3);
+        transform: rotate(1turn);
+        transition-duration: 2s;
+        transition-timing-function: cubic-bezier(0.34, 0, 0.84, 1);
       }
     }
-    
 
     ul {
       display: none;
@@ -138,26 +138,22 @@ header.login {
       list-style: none;
       border: 1px solid #eaeaea;
       border-radius: 5px;
-      margin:0;
+      margin: 0;
       padding: 0;
       background-color: #fff;
-     
+
       a {
         text-decoration: none;
         color: #333;
         font-size: 16px;
         display: block;
         padding: 10px 20px;
-        white-space:nowrap;
+        white-space: nowrap;
 
         &:hover {
           background-color: #eaeaea;
         }
-      
       }
-
-
-
     }
 
     &:hover ul {
@@ -165,5 +161,4 @@ header.login {
     }
   }
 }
-
 </style>
