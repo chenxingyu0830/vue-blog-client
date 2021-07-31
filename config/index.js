a4a9ee70de10a8d84ab32f345a2435e556542974
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-07-31 22:07:45 +0800

    Feat: update dist

--- config/index.js
+++ config/index.js
@@ -42,8 +42,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: './assets/',
-    assetsPublicPath: '../../',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
